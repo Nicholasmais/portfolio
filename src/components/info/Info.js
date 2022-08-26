a4e3fc8e76ff885bb06dd478146315b1ba5eb044
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2022-08-26 12:04:55 -0300

    Escopo inicial

---
+++ src/components/info/Info.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./Info.module.scss"
+
+const Info = () => {
+  return (
+    <div className={s.main}>About me</div>
+  )
+}
+
+export default Info
---
+++ src/components/presentation/Presentation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from "./Presentation.module.scss"
+
+const Presentation = () => {
+  return (
+    <div className={s.main}>
+        Presentation
+    </div>
+  )
+}
+
+export default Presentation
