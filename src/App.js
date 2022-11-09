c768ccd96d0367097cab54e980491d4b811b3c80
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2022-11-09 15:33:20 -0300

    Footer working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import s from './App.scss';
 import Context from './components/context/Context';
+import Footer from './components/footer/Footer';
 import Info from './components/info/Info';
 import Navigation from './components/navigation/Navigation';
 import Presentation from './components/presentation/Presentation';
@@ -15,6 +16,10 @@ function App() {
       </BrowserRouter>
       <Presentation></Presentation>
       <Info></Info>
+
+      <footer>
+        <Footer></Footer>
+      </footer>
     </>
   );
 }
